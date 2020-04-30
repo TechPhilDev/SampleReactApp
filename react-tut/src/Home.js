d6090f24fba06cedd1fd4d9490f31bb93cb1973d
TechPhil <26381564+TechPhil@users.noreply.github.com>
2020-04-30 22:35:37 +0100

    Upload of files

---
+++ react-tut/src/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render() {
+        return (
+            <div>
+                <h2>Welcome to TechPhil</h2>
+                <p>Welcome to the TechPhil website!!</p>
+                <p>You are currently listening to Simulator Radio. For more information, please click on the link in the navbar</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ react-tut/src/SR.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SR extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Simulator Radio</h2>
+                <p>Right now, you're listening to Simulator Radio!</p>
+                <p>If not, why not? Visit simulatorradio.com, or click the button in the top right to open the player</p>
+            </div>
+        )
+    }
+}
