948ffb4de24088f9edacf9c92b23a8e3ce089c6c
TechPhil <26381564+TechPhil@users.noreply.github.com>
2020-04-30 23:35:43 +0100

    Updated working example with correct CSS and Navbar

--- react-tut/src/Nav.js
+++ react-tut/src/Nav.js
@@ -5,7 +5,7 @@ class Nav extends Component{
     render() {
         return(
             <ul className="header">
-    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink exact to="/">Home</NavLink></li>
     <li><NavLink to="/sr">SR</NavLink></li>
     <li><NavLink to="/contact">Contact</NavLink></li>
 </ul>
